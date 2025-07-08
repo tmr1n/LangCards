99725209fb21fa105b4dad890a36fa19c9a584c9
Chinyonov-Vladislav <vlad2000100600@gmail.com>
2025-07-08 17:27:15 +0300

    Фикс ошибки

--- Backend/app/Http/Controllers/Api/V1/DeckController.php
+++ Backend/app/Http/Controllers/Api/V1/DeckController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\FiltersForModels\DeckFilter;
-use App\Http\Resources\v1\DeckResources\DeckCollectionResource;
 use App\Http\Resources\v1\DeckResources\DeckResource;
 use App\Http\Responses\ApiResponse;
 use App\Repositories\DeckRepositories\DeckRepositoryInterface;
